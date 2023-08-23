9edb534043a55832c370a11dc98bf71fa80ecf56
krisMcGonegal <krismcgonegal@gmail.com>
2023-08-23 18:34:12 -0400

    First commit

---
+++ src/components/NewCatalogue.js
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const NewCatalogue = () => {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect ( () => {
+
+        axios({
+            url: `https://api.themoviedb.org/3/discover/movie`,
+            params: {
+                api_key: process.env.REACT_APP_API_KEY,
+                language: 'en-US',
+                sort_by: 'popularity.desc',
+                include_adult: 'false',
+                include_video: 'false',
+                region: 'us',
+                page: 1,
+                primary_release_year: 1985,
+            },
+        }).then((results) => {
+            const apiResults = (results.data.results)
+            setMovies(apiResults)
+        })
+    }, [])
+
+    return (
+        <>
+            <ul className = "catalogue">
+                {
+                    movies.map((movie) => {
+                        return (
+                        <li key={movie.id} data-movieid={movie.id}>
+                            <Link to={`/movie/${movie.id}`}>
+                                <img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={`Poster for ${movie.original_title}`} />
+                            </Link>
+                        </li>
+                        )
+                    })
+                }
+            </ul>
+        
+        </>
+    )
+}
+
+export default NewCatalogue;
